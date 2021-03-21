fc4226d0eabf840eeb56581d267ff94f31087f4a
bennyfajri <m.b.fajrii@gmail.com>
2021-03-22 00:05:39 +0700

    add splash screen

---
+++ app/src/main/java/com/benny/resin_ku/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.benny.resin_ku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //Make splash screen as a full screen activity
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        //postDelayed method to send a messade with a delay time
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000) //2000 in milliseconds
+    }
+}
